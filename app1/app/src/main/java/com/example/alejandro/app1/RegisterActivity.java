11b41446abda8ac4e58dae3a3b594c3835f8946f
aaguilar1 <aa1363@scarletmail.rutgers.edu>
2017-03-16 17:45:02 -0400

    Create on runtime second page

--- app1/app/src/main/java/com/example/alejandro/app1/RegisterActivity.java
+++ app1/app/src/main/java/com/example/alejandro/app1/RegisterActivity.java
@@ -1,5 +1,42 @@
 package com.example.alejandro.app1;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.app.LoaderManager.LoaderCallbacks;
+
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.Manifest.permission.READ_CONTACTS;
+
+
 /**
  * Created by alejandro on 3/16/2017.
  */
@@ -7,7 +44,26 @@ package com.example.alejandro.app1;
 public class RegisterActivity extends LoginActivity{
 
 
-    String h = "Hello world";
+    //String h = "Hello world";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        // Set up the login form.
+        // mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        //populateAutoComplete();
+
+       /* mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }*/
+        }
 
 
 
