37473c3e0a96f15a5583871873e52862c2a71ab0
aaguilar1 <aa1363@scarletmail.rutgers.edu>
2017-03-16 17:16:25 -0400

    initial commit

---
+++ app1/app/src/main/java/com/example/alejandro/app1/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.alejandro.app1;
+
+/**
+ * Created by alejandro on 3/16/2017.
+ */
+
+public class RegisterActivity extends LoginActivity{
+
+
+    String h = "Hello world";
+
+
+
+
+
+
+
+
+
+
+}
