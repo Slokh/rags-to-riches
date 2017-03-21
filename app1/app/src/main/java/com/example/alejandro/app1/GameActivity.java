edab8216f684fec7a7c18925f567c46411b5ca6d
KartikPatel6 <kp605@scarletmail.rutgers.edu>
2017-03-20 22:08:54 -0400

    Added a simple list view of companies with dummy buttons

--- app1/app/src/main/java/com/example/alejandro/app1/GameActivity.java
+++ app1/app/src/main/java/com/example/alejandro/app1/GameActivity.java
@@ -1,22 +1,49 @@
 package com.example.alejandro.app1;
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.example.alejandro.app1.adapters.CompanyAdapter;
+import com.example.alejandro.app1.models.Company;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Kartik on 3/20/2017.
  */
 
-public class GameActivity extends LoginActivity{
+public class GameActivity extends AppCompatActivity {
 
+    ArrayAdapter<Company> displayAdapter;
     private Button mQuitButton = null;
+    List<Company> companies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        companies = new ArrayList<Company>();
+        companies.add(new Company("Company A", "A", "A company.", "Apple", 50));
+        companies.add(new Company("Company B", "B", "B company.", "Apple", 50));
+        companies.add(new Company("Company C", "C", "C company.", "Apple", 50));
+        companies.add(new Company("Company D", "D", "D company.", "Apple", 50));
+        companies.add(new Company("Company E", "E", "E company.", "Apple", 50));
+
+        Context context = getApplicationContext();
+        displayAdapter = new CompanyAdapter(GameActivity.this, context, companies);
+
+        ListView listView = (ListView) findViewById(R.id.companyList);
+        listView.setAdapter(displayAdapter);
 
         mQuitButton = (Button) findViewById(R.id.quitButton);
         mQuitButton.setOnClickListener(new View.OnClickListener() {
---
+++ app1/app/src/main/java/com/example/alejandro/app1/adapters/CompanyAdapter.java
@@ -0,0 +1,64 @@
+package com.example.alejandro.app1.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.alejandro.app1.R;
+import com.example.alejandro.app1.models.Company;
+
+import java.util.List;
+
+import static android.R.attr.path;
+
+/**
+ * Created by Kartik on 3/20/2017.
+ */
+
+public class CompanyAdapter extends GenericArrayAdapter<Company> {
+
+    private List<Company> companies;
+    private Activity activity;
+
+    public CompanyAdapter(Activity activity, Context context, List<Company> companies) {
+        super(context, companies);
+        this.activity = activity;
+    }
+
+    @Override
+    public void drawText(TextView textView, Company object) {
+
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        View vi = convertView;
+        if (vi == null)
+            vi = mInflater.inflate(R.layout.layout_listrow, null);
+        TextView text = (TextView) vi.findViewById(R.id.companyText);
+        Button buyButton = (Button) vi.findViewById(R.id.companyBuyButton);
+        Button sellButton = (Button) vi.findViewById(R.id.companySellButton);
+
+        text.setText(data.get(position).getName());
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        sellButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        return vi;
+    }
+}
