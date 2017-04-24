2633eb5639d2ae104c63939836fa13c3192ff7bf
Slokh <slokexx@gmail.com>
2017-04-24 15:12:27 -0400

    Minor text changes

--- app1/app/src/main/java/com/example/alejandro/app1/IntroActivity.java
+++ app1/app/src/main/java/com/example/alejandro/app1/IntroActivity.java
@@ -27,33 +27,19 @@ public class IntroActivity extends AppIntro {
         final Bundle extras = getIntent().getExtras();
         account = new Account(extras.getInt("id"), extras.getString("email"), extras.getString("username"), extras.getString("password"));
 
-        // Note here that we DO NOT use setContentView();
-
-        // Add your slide fragments here.
-        // AppIntro will automatically generate the dots indicator and buttons.
-        // addSlide(firstFragment);
-        // addSlide(secondFragment);
-        // addSlide(thirdFragment);
-        // addSlide(fourthFragment);
-
-        // Instead of fragments, you can also use our default slide
-        // Just set a title, description, background and image. AppIntro will do the rest.
         addSlide(AppIntroFragment.newInstance("Welcome!", "Welcome to Rags To Riches!\n A game designed to help you get acclimated with the stock market.", R.drawable.ragstoricheslogo, Color.parseColor("#4caf50")));
         addSlide(AppIntroFragment.newInstance("Game Navigation", "Use one of the game buttons to start playing!", R.drawable.intro2, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Options", "Track your achievement progress\n or modify your game settings", R.drawable.intro3, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Achievements", "Try to obtain these achievements to measure your progress", R.drawable.achievements_page, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Game Overlay", "Balance: The money you have to spend\nPortfolio: The stocks you own\nNext Turn: Advance one week in the game", R.drawable.intro4, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Company Options", "Invest in a company by buying a piece of stock\nSell a company's stock to get a monetary return\nUse the available information to aid your decision", R.drawable.intro5, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Trends", "Use each company's stock price trends to infer the future of the company", R.drawable.intro6, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Goal", "You have 25 turns to make as much money as possible\nAll stocks owned will be sold at the end", R.drawable.intro7, Color.parseColor("#4caf50")));
-        addSlide(AppIntroFragment.newInstance("Results", "Congratulations (or not)!\nAll players and their end-game balance will be shown", R.drawable.intro8, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Options", "Track your achievement progress\n or modify your game settings.", R.drawable.intro3, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Achievements", "Try to obtain achievements to measure your progress.", R.drawable.achievements_page, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Game Overlay", "Balance: The money you have to spend\n\nPortfolio: The stocks you own\n\nNext Turn: Advance one week in the game", R.drawable.intro4, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Company Options", "Invest in a company by buying a piece of stock.\n\nSell a company's stock to get a monetary return.\n\nUse the available information to aid your decision.", R.drawable.intro5, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Trends", "Use each company's stock price trends to infer the future of the company.", R.drawable.intro6, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Goal", "You have 25 turns to make as much money as possible.\nAll stocks owned at the end of the game will be sold.", R.drawable.intro7, Color.parseColor("#4caf50")));
+        addSlide(AppIntroFragment.newInstance("Results", "Congratulations (if you have won)!\n\nHave fun!", R.drawable.intro8, Color.parseColor("#4caf50")));
 
-        // OPTIONAL METHODS
-        // Override bar/separator color.
         setBarColor(Color.parseColor("#29702c"));
         setSeparatorColor(Color.parseColor("#388e3c"));
 
-        // Hide Skip/Done button.
         showSkipButton(true);
         showStatusBar(false);
         setVibrate(false);
@@ -62,7 +48,6 @@ public class IntroActivity extends AppIntro {
     @Override
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
-        // Do something when users tap on Skip button.
         Intent i = new Intent(getApplicationContext(), MainMenuActivity.class);
         i.putExtra("id", account.getId());
         i.putExtra("email", account.getEmail());
@@ -74,7 +59,6 @@ public class IntroActivity extends AppIntro {
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
-        // Do something when users tap on Done button.
         Intent i = new Intent(getApplicationContext(), MainMenuActivity.class);
         i.putExtra("id", account.getId());
         i.putExtra("email", account.getEmail());
@@ -86,6 +70,5 @@ public class IntroActivity extends AppIntro {
     @Override
     public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
         super.onSlideChanged(oldFragment, newFragment);
-        // Do something when the slide changes.
     }
 }
